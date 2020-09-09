31d57945c9cdc5845c92850711eed95e36f6c958
settouche <adel.settouche@epsi.fr>
2020-09-09 16:49:46 +0200

    Formulaire et Tableau

--- Panelclient.php
+++ Panelclient.php
@@ -1,1 +1,32 @@
 <?php include ("header.php") ?>
+
+<h1>Panel client</h1>
+<br>
+<h3>Votre consommation</h3>
+<div class="consommation"></div>
+
+<div class="tab1">
+    <h4>Le rapport de l'expert</h4>
+    <table class="table table table-dark table-responsive">
+        <tr>
+            <th>Document importé </th>
+            <th>Date</th>
+        </tr>
+
+
+    </table>
+</div>
+
+
+
+
+<div class="tab2">
+    <h5>Appareil nomade à aquérir </h5>
+    <table class="table table table-dark table-responsive">
+        <tr>
+            <th>Nom de l'appareil </th>
+            <th>Prix</th>
+        </tr>
+
+    </table>
+</div>
